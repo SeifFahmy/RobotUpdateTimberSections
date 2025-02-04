8deb9789058f189f26cdebc425923a9ecfb4a5cb
Seif (Cundall) <seifehab.bl4@gmail.com>
2025-02-04 15:13:53 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using RobotOM;
+using Newtonsoft.Json;
+
+namespace RobotUpdateTimberSections
+{
+    class Program
+    {
+        /// <summary>
+        /// Accepts section data in the format:
+        /// {
+        ///     "section": [list of bar IDs],
+        ///     "180x320": [1, 2, 3],
+        ///     "360x1000": [4, 5, 6],
+        /// }
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                System.Console.WriteLine("Invalid number of arguments provided");
+                return;
+            }
+
+            var jsonData = args[0];
+            var groupedIds = JsonConvert.DeserializeObject<Dictionary<string, List<int>>>(jsonData);
+            if (groupedIds == null)
+            {
+                Console.WriteLine("Invalid number of arguments provided");
+                return;
+            }
+
+            RobotApplication robotApp = new();
+            RobotLabelServer robotLabelServer = robotApp.Project.Structure.Labels;
+
+            foreach (var (section, idsList) in groupedIds)
+            {
+                var splitSectionName = section.Split('x').ToList();
+                double width = double.Parse(splitSectionName[0]);
+                double depth = double.Parse(splitSectionName[1]);
+
+                // Add section to the model's currently used sections
+                IRobotLabel sectionLabel = robotLabelServer.Create(IRobotLabelType.I_LT_BAR_SECTION, $"Timber {section}");
+                RobotBarSectionData sectionData = (RobotBarSectionData)sectionLabel.Data;
+                sectionData.MaterialName = "TIMBER";
+                sectionData.Type = IRobotBarSectionType.I_BST_NS_RECT;
+                sectionData.ShapeType = IRobotBarSectionShapeType.I_BSST_RECT_FILLED;
+                RobotBarSectionNonstdData nonStandardSectionData = sectionData.CreateNonstd(0); // parametric section
+
+                nonStandardSectionData.SetValue(IRobotBarSectionNonstdDataValue.I_BSNDV_RECT_B, width / 1000);
+                nonStandardSectionData.SetValue(IRobotBarSectionNonstdDataValue.I_BSNDV_RECT_H, depth / 1000);
+                sectionData.CalcNonstdGeometry();
+                robotLabelServer.Store(sectionLabel);
+
+                // Assign the section to the appropriate group of bars
+                RobotSelection barSelection = robotApp.Project.Structure.Selections.Create(IRobotObjectType.I_OT_BAR);
+                string ids = string.Join(" ", idsList);
+                barSelection.FromText(ids);
+                robotApp.Project.Structure.Bars.SetLabel(barSelection, IRobotLabelType.I_LT_BAR_SECTION, sectionLabel.Name);
+            }
+        }
+    }
+}
